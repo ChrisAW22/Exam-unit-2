d7ee13f8154a82f3c1f65472c4bf7bc91b7df955
ChrisAW22 <cwalther514@gmail.com>
2025-02-18 22:27:28 +0100

    commit

---
+++ moduleSkillSummary_assignment-main/moduleSkillSummary_assignment-main/functions.mjs
@@ -0,0 +1,30 @@
+function square(x) {
+    return x * x;
+}
+
+function millimeter(x) {
+    return x * 25.4;
+}
+
+function root(x) {
+    return Math.sqrt(x);
+}
+
+function cube(x) {
+    return x * x * x;
+}
+
+function area(x) {
+    return Math.PI * (x * x);
+}
+
+function greeting(name) {
+    return ("Hello " + name);
+}
+
+console.log(square(4));
+console.log(millimeter(100));
+console.log(root(16));
+console.log(cube(3));
+console.log(area(6));
+console.log(greeting("Charlie"));
