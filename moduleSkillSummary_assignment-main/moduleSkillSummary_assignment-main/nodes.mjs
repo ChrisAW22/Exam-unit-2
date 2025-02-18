673ce1b73324711149fe4b9f86b434bdc61e8639
ChrisAW22 <cwalther514@gmail.com>
2025-02-18 21:17:12 +0100

    commit

--- moduleSkillSummary_assignment-main/moduleSkillSummary_assignment-main/nodes.mjs
+++ moduleSkillSummary_assignment-main/moduleSkillSummary_assignment-main/nodes.mjs
@@ -1,21 +1,178 @@
-import data from "./example_files/nodes.json";
-
 function max(a, b) {
     return a > b ? a : b;
 }
 
-function deepestLevel(tree, level, max, node) {
+function deepestLevel(tree, level, max) {
     if (tree.left) {
-        node, max = deepestLevel(tree.left, level + 1, max);
+        max = deepestLevel(tree.left, level + 1, max);
     }
     if (tree.right) {
-        node, max = deepestLevel(tree.right, level + 1, max);
+        max = deepestLevel(tree.right, level + 1, max);
     }
     if (max < level) {
         max = level;
-        node = tree.value;
     }
-    return node, max;
+    return max;
 }
-
+var data = {
+    "value": 281,
+    "left": null,
+    "right": {
+        "value": 168,
+        "left": {
+            "value": 732,
+            "left": {
+                "value": 653,
+                "left": {
+                    "value": 372,
+                    "left": null,
+                    "right": {
+                        "value": 879,
+                        "left": null,
+                        "right": {
+                            "value": 330,
+                            "left": {
+                                "value": 752,
+                                "left": null,
+                                "right": {
+                                    "value": 328,
+                                    "left": null,
+                                    "right": {
+                                        "value": 278,
+                                        "left": null,
+                                        "right": {
+                                            "value": 983,
+                                            "left": {
+                                                "value": 924,
+                                                "left": {
+                                                    "value": 269,
+                                                    "left": {
+                                                        "value": 558,
+                                                        "left": {
+                                                            "value": 571,
+                                                            "left": null,
+                                                            "right": {
+                                                                "value": 241,
+                                                                "left": null,
+                                                                "right": null
+                                                            }
+                                                        },
+                                                        "right": null
+                                                    },
+                                                    "right": null
+                                                },
+                                                "right": null
+                                            },
+                                            "right": {
+                                                "value": 912,
+                                                "left": null,
+                                                "right": {
+                                                    "value": 438,
+                                                    "left": {
+                                                        "value": 977,
+                                                        "left": null,
+                                                        "right": {
+                                                            "value": 119,
+                                                            "left": null,
+                                                            "right": {
+                                                                "value": 410,
+                                                                "left": null,
+                                                                "right": {
+                                                                    "value": 97,
+                                                                    "left": null,
+                                                                    "right": {
+                                                                        "value": 906,
+                                                                        "left": null,
+                                                                        "right": null
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    },
+                                                    "right": {
+                                                        "value": 971,
+                                                        "left": null,
+                                                        "right": {
+                                                            "value": 441,
+                                                            "left": null,
+                                                            "right": null
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            },
+                            "right": null
+                        }
+                    }
+                },
+                "right": null
+            },
+            "right": {
+                "value": 364,
+                "left": {
+                    "value": 131,
+                    "left": null,
+                    "right": {
+                        "value": 125,
+                        "left": null,
+                        "right": {
+                            "value": 895,
+                            "left": {
+                                "value": 910,
+                                "left": null,
+                                "right": {
+                                    "value": 838,
+                                    "left": {
+                                        "value": 12,
+                                        "left": null,
+                                        "right": {
+                                            "value": 703,
+                                            "left": {
+                                                "value": 809,
+                                                "left": null,
+                                                "right": null
+                                            },
+                                            "right": null
+                                        }
+                                    },
+                                    "right": null
+                                }
+                            },
+                            "right": {
+                                "value": 86,
+                                "left": null,
+                                "right": {
+                                    "value": 429,
+                                    "left": null,
+                                    "right": {
+                                        "value": 547,
+                                        "left": null,
+                                        "right": {
+                                            "value": 545,
+                                            "left": null,
+                                            "right": {
+                                                "value": 267,
+                                                "left": {
+                                                    "value": 652,
+                                                    "left": null,
+                                                    "right": null
+                                                },
+                                                "right": null
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                "right": null
+            }
+        },
+        "right": null
+    }
+}
 console.log(deepestLevel(data, null, null, null));
