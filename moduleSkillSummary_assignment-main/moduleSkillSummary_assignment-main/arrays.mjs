492c84cd6cc9396c5d052bbd8ca9bae4b411d8ee
ChrisAW22 <cwalther514@gmail.com>
2025-02-18 23:06:32 +0100

    commit

--- moduleSkillSummary_assignment-main/moduleSkillSummary_assignment-main/arrays.mjs
+++ moduleSkillSummary_assignment-main/moduleSkillSummary_assignment-main/arrays.mjs
@@ -1,2 +1,14 @@
 import { arrays } from "./data.mjs";
 
+function flatArray (array, result = []) {
+    for (const i of array) {
+        if (Array.isArray(i)) {
+            flatArray(i, result)
+        } else {
+            result.push(i);
+            }
+    }
+    return result;
+}
+
+console.log(flatArray(arrays, []));
