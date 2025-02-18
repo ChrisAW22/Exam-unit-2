c771942f7441ecd9a4914117d5c36f745d4a9485
ChrisAW22 <cwalther514@gmail.com>
2025-02-19 00:06:27 +0100

    commit

---
+++ moduleSkillSummary_assignment-main/moduleSkillSummary_assignment-main/books.mjs
@@ -0,0 +1,64 @@
+function books(books, result, author) {
+    for (const book of books) {
+        const part = books.title.slice(0, 2)
+        if (part === "The") {
+            result.the.push(book);
+        }
+        if (book.author.match("t")) {
+            result.t_authors.push(book);
+        }
+        if (book.publication_year > 1992) {
+            result.after_1992.push(book);
+        }
+        if (book.publication_year < 2004) {
+            result.before_2004.push(book);
+        }
+        if (book.author === author) {
+            result.isbn.push(book.isbn);
+        }
+    }
+    return result;
+    
+}
+
+let book_result = {
+    the: [],
+    t_authors: [],
+    after_1992: [],
+    before_2004: [],
+    isbn: [],
+}
+
+function asc_books_name(books) {
+    books.sort((a, b) => a.title - b.title);
+}
+
+function desc_books_name(books) {
+    books.sort((a, b) => b.title - a.title);
+}
+
+function asc_books_year(books) {
+    books.sort((a, b) => a.publication_year - b.publication_year);
+}
+
+function desc_books_year(books) {
+    books.sort((a, b) => b.publication_year - a.publication_year);
+}
+
+function asc_books_first_name(books) {
+    books.sort((a, b) => a.title - b.title);
+}
+
+function desc_books_first_name(books) {
+    books.sort((a, b) => b.author - a.author);
+}
+
+function asc_books_last_name(books) {
+    books.sort((a, b) => a.author.split()[1] - b.author.split()[1]);
+}
+
+function desc_books_last_name(books) {
+    books.sort((a, b) => b.author.split()[1] - a.author.split()[1]);
+}
+
+console.log()
